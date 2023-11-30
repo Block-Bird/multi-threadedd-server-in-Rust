2706fc58f49789e23f075af28777112a01208f6c
Asad Ali <asad.ali@invozone.com>
2023-11-30 15:09:13 +0500

    code optimization

--- src/main.rs
+++ src/main.rs
@@ -1,65 +1,51 @@
 // use std::arch::x86_64::_mm256_mask_cvtusepi64_storeu_epi8;
 use std::f32::consts::E;
-use std::fs::{*, self};
-use std::net::{TcpListener , TcpStream};
+use std::fs::{self, *};
 use std::io::prelude::*;
+use std::net::{TcpListener, TcpStream};
 fn main() {
-    let listerner = TcpListener::bind("127.0.0.1:7878").unwrap(); 
+    let listerner = TcpListener::bind("127.0.0.1:7878").unwrap();
 
-    // let incoming_stream = listerner.incoming() ; 
+    // let incoming_stream = listerner.incoming() ;
     for incoming_stream in listerner.incoming() {
-        
         match incoming_stream {
-
             Ok(incoming_stream) => {
                 println!("Connection Established");
                 handle_connection(incoming_stream);
             }
             Err(e) => {
-                println!("Erro is Connection Establishing {:?} ", e); 
+                println!("Erro is Connection Establishing {:?} ", e);
             }
-            
         }
-        
     }
-
 }
 
-fn handle_connection(mut stream : TcpStream  )  {
-    let mut buffer = [0; 1024]; 
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
     stream.read(&mut buffer).unwrap();
-    
-    let get = b"GET / HTTP/1.1\r\n"; 
 
-    if buffer.starts_with(get) {
+    let get = b"GET / HTTP/1.1\r\n";
 
-        println!("Printing If Condition");
+    let (status_line, file_name) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK", "index.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND", "404.html")
+    };
 
-        let contents: String = fs::read_to_string("index.html").unwrap(); 
-        let response = format!(
-            "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}", 
-            contents.len(), 
-            contents
-        );
-    
-        stream.write(response.as_bytes()).unwrap();
-        stream.flush().unwrap();
+    let contents: String = fs::read_to_string(file_name).unwrap();
 
-    }
-    else {
+    let response = format!(
+        "{}\r\n Content-Length: {}\r\n\r\n{}",
+        status_line,
+        contents.len(),
+        contents
+    );
 
-        println!("Printing Else Condition");
-        let content = fs::read_to_string("404.html").unwrap(); 
+    //         contents.len(),
+    //         contents
+    //     );
 
-        let response = format!(
-            "HTTP/1.1 404 NOT FOUND\r\nContent-Length: {}\r\n\r\n{}", 
-            content.len(), 
-            content
-        );
-
-        stream.write(response.as_bytes()).unwrap(); 
-
-    }
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
 
 }
- 
